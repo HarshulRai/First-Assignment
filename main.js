79d87d9f9db36b50f620c318a86a2fc8f5308083
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2022-07-18 22:17:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const ul = document.querySelector('.items');
+ul.firstElementChild.textContent = 'Hello'; 
+ul.firstElementChild.style.color = 'green';
+ul.children[1].innerText = 'Brad';
+ul.children[1].style.color = 'yellow';
